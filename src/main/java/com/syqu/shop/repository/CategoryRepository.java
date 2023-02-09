98c61bab1a19931c5ab4d7a4d8d59ea7b2e5ae13
Siraj <sirajhu4@gmail.com>
2023-02-09 18:54:57 +0530

    new java file

--- src/main/java/com/syqu/shop/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.syqu.shop.repository;
-
-import com.syqu.shop.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByCategoryName(String name);
-}
